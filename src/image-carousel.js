747536a3a7ea3ad6bcf11a98feb1e891f319f833
Cameron Brown <moontsukuyomi6@gmail.com>
2025-07-04 10:29:47 +0200

    Add a function to handle image carousel intervals and add a function to handle image carousel button events

---
+++ src/image-carousel.js
@@ -0,0 +1,59 @@
+const handleImageCarousel = () => {
+  const imageOne = document.querySelector('.slide__img-1');
+  const imageTwo = document.querySelector('.slide__img-2');
+  const imageThree = document.querySelector('.slide__img-3');
+  const navDotOne = document.querySelector('.nav-dot-1');
+  const navDotTwo = document.querySelector('.nav-dot-2');
+  const navDotThree = document.querySelector('.nav-dot-3');
+  let intervalId;
+  const imageCarouselRotation = () => {
+    intervalId = setInterval(() => {
+      if (imageOne.classList.contains('slide__img--active')) {
+        imageOne.classList.remove('slide__img--active');
+        imageTwo.classList.add('slide__img--active');
+        navDotOne.classList.remove('nav-dot--active');
+        navDotTwo.classList.add('nav-dot--active');
+      } else if (imageTwo.classList.contains('slide__img--active')) {
+        imageTwo.classList.remove('slide__img--active');
+        imageThree.classList.add('slide__img--active');
+        navDotTwo.classList.remove('nav-dot--active');
+        navDotThree.classList.add('nav-dot--active');
+      } else if (imageThree.classList.contains('slide__img--active')) {
+        imageThree.classList.remove('slide__img--active');
+        imageOne.classList.add('slide__img--active');
+        navDotThree.classList.remove('nav-dot--active');
+        navDotOne.classList.add('nav-dot--active');
+      }
+    }, 5000);
+  };
+
+  const imageCarouselEvents = () => {
+    const navDot = document.querySelectorAll('.nav-dot');
+    const carouselImages = document.querySelectorAll('.slide__img');
+    navDot.forEach((dot) => {
+      dot.addEventListener('click', () => {
+        carouselImages.forEach((image) => {
+          image.classList.remove('slide__img--active');
+        });
+        navDot.forEach((navD) => {
+          navD.classList.remove('nav-dot--active');
+        });
+        if (dot.classList.contains('nav-dot-1')) {
+          imageOne.classList.add('slide__img--active');
+          navDotOne.classList.add('nav-dot--active');
+        } else if (dot.classList.contains('nav-dot-2')) {
+          imageTwo.classList.add('slide__img--active');
+          navDotTwo.classList.add('nav-dot--active');
+        } else if (dot.classList.contains('nav-dot-3')) {
+          imageThree.classList.add('slide__img--active');
+          navDotThree.classList.add('nav-dot--active');
+        }
+        clearInterval(intervalId);
+        imageCarouselRotation();
+      });
+    });
+  };
+  return { imageCarouselRotation, imageCarouselEvents };
+};
+
+export default handleImageCarousel;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './styles.css';
 import handleDropdownMenu from './dropdown-menu';
+import handleImageCarousel from './image-carousel';
 
 handleDropdownMenu();
+const carousel = handleImageCarousel();
+carousel.imageCarouselRotation();
+carousel.imageCarouselEvents();
